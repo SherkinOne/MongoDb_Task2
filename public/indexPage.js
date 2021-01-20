885b1fc0e4e2b6ae1e9beabaa518964bc46bdf0b
K <25370141+SherkinOne@users.noreply.github.com>
2021-01-20 21:02:52 +0000

    Add ejs

---
+++ public/indexPage.js
@@ -0,0 +1,7 @@
+
+
+window.addEventListener('load', function () {
+    alert("It's loaded!")
+    var questionText=document.getElementById("answerThis");
+questionText.innerHTML="dsfsdf";
+  })
--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const path = require('path');
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 
-
 // Make sure you place body-parser before your CRUD handlers!
 app.use(bodyParser.urlencoded({ extended: true }))
+//Serves all the request which includes /images in the url from Images folder
+// app.use('/images', express.static(__dirname + '/Images'));
 
-app.use(express.static("/public/"))
-app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, '/public')))
+
 
 
 MongoClient.connect("mongodb+srv://admin:t4pwM36QyAe4F9p!@coursecluster.pzjcy.mongodb.net/Quiz?retryWrites=true&w=majority", { useUnifiedTopology: true
@@ -27,16 +28,16 @@ console.log('listening on 3000')
 })
 
 app.get('/', (req, res) => {
-    // res.sendFile(__dirname + '/index.html')
-  
-        db.collection('questions').find().toArray()
-        .then(results => {
-        console.log(results)
-        })
-        .catch(error => console.error(error))
-        res.render('index.ejs', { quotes: results })
+  // res.sendFile(__dirname + '/index.html')
 
-    })  
+      db.collection('questions').find().toArray()
+      .then(results => {
+      console.log(results)
+      })
+      .catch(error => console.error(error))
+      res.render('index.ejs', { quotes: results })
+
+  })  
 
 
     app.post('/addQuestion', (req, res) => {
