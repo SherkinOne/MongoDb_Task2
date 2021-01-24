d53aedc2fcdf2a3d3406bf02d4897a357f79ce64
K <25370141+SherkinOne@users.noreply.github.com>
2021-01-24 17:40:14 +0000

    Can add a new document

--- public/indexPage.js
+++ public/indexPage.js
@@ -1,7 +1,49 @@
+window.addEventListener("load", function () {
+  // const addNewAnswerButton=document.getElementById('addNewAnswer');
+
+  // ADD NEW ANSWER PAGE FUNCTIONS
+
+  // add new answer
+  //   addNewAnswerButton.addEventListener('click', function(e) {
+  // document.getElementById("newAnswerSection").innerHTML+='<label class="">  Other Answer 2 </label> <input class="answers  w-80 mb-4 border-4 border-blue-500 border-opacity-50" />'
+  //   });
 
 
-window.addEventListener('load', function () {
-    alert("It's loaded!")
-    var questionText=document.getElementById("answerThis");
-questionText.innerHTML="dsfsdf";
-  })
+  //  ---        - end of enser page functions --
+
+  const button = document.getElementById("answerButton");
+
+  button.addEventListener("click", function (e) {
+    console.log("button was clicked");
+
+    fetch("/answer", { method: "GET" })
+      .then(function (response) {
+        if (response.ok) {
+          console.log("Click was recorded");
+
+          return;
+        }
+        throw new Error("Request failed.");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  });
+
+  // changeButton.addEventListener('click', function(e) {
+  //   console.log('button was clicked');
+
+  //   fetch('/change', {method: 'GET'})
+  //     .then(function(response) {
+  //       if(response.ok) {
+  //         console.log('Click was recorded');
+
+  //         return;
+  //       }
+  //       throw new Error('Request failed.');
+  //     })
+  //     .catch(function(error) {
+  //       console.log(error);
+  //     });
+  // });
+});
--- server.js
+++ server.js
@@ -1,46 +1,70 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const bodyParser= require('body-parser')
-const MongoClient = require('mongodb').MongoClient
+const path = require("path");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const router = express.Router();
 
 // Make sure you place body-parser before your CRUD handlers!
-app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.urlencoded({ extended: true }))
 //Serves all the request which includes /images in the url from Images folder
 // app.use('/images', express.static(__dirname + '/Images'));
 
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
+MongoClient.connect(
+  "mongodb+srv://admin:t4pwM36QyAe4F9p!@coursecluster.pzjcy.mongodb.net/Quiz?retryWrites=true&w=majority",
+  { useUnifiedTopology: true }
+)
+  .then((client) => {
+    console.log("Connected to Database");
+    const db = client.db("Quiz");
 
+    app.listen(3000, function () {
+      console.log("listening on 3000");
+    });
 
-MongoClient.connect("mongodb+srv://admin:t4pwM36QyAe4F9p!@coursecluster.pzjcy.mongodb.net/Quiz?retryWrites=true&w=majority", { useUnifiedTopology: true
-})
-.then(client => {
-console.log('Connected to Database')
-})
-.catch(error => console.error(error))
+    app.get("/", (req, res) => {
+      res.render("index.ejs");
+    });
 
-
-
-
-app.listen(3000, function() {
-console.log('listening on 3000')
-})
-
-app.get('/', (req, res) => {
-  // res.sendFile(__dirname + '/index.html')
-
-      db.collection('questions').find().toArray()
-      .then(results => {
-      console.log(results)
-      })
-      .catch(error => console.error(error))
-      res.render('index.ejs', { quotes: results })
-
-  })  
-
-
-    app.post('/addQuestion', (req, res) => {
-        console.log('Hellooooooooooooooooo!')
+    app.get("/answer", (req, res) => {
+      console.log("here");
+      db.collection("Quiz")
+        .find()
+        .toArray()
+        .then((results) => {
+          console.log(results);
+          res.render("answer.ejs", { quotes: results });
         })
-        
+        .catch((error) => console.error(error));
+    });
+
+// save new question
+    app.post("/savenewquestion" ,(req, res) => {
+      console.log("here")
+      console.log("body " ,req.body);
+      db.collection("Quiz").insertOne(req.body)
+      res.redirect("/addnew");
+    });
+
+
+
+    app.get("/change", (req, res) => {
+      console.log("change");
+      //  res.sendFile(__dirname +'/views/addnew.html')
+
+      res.render("./change.ejs");
+    });
+
+    app.get("/addnew", (req, res) => {
+      console.log("addnew");
+      //  res.sendFile(__dirname +'/views/addnew.html')
+
+      res.render("./addnew.ejs");
+    });
+  })
+  .catch((error) => console.error(error));
