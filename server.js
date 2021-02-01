f2cae24ca6c2ee35494a72a6413c8c352191af4c
SherkinOne <25370141+SherkinOne@users.noreply.github.com>
2021-02-01 20:23:12 +0000

    changed port listener

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ MongoClient.connect(
     console.log("Connected to Database");
     const db = client.db("Quiz");
 
-    app.listen(3000, function () {
+    app.listen(process.env.PORT || 3000, function () {
       console.log("listening on 3000");
     });
 
